bab1c010501c7d26129d2328fbb97bc4553b3a43
Moumit Mondal <moumit.mondal@gmail.com>
2024-03-06 22:22:23 +0530

    Dynamic bean from application properites

--- src/main/java/com/devglan/Application.java
+++ src/main/java/com/devglan/Application.java
@@ -2,14 +2,18 @@ package com.devglan;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 import com.zaxxer.hikari.HikariDataSource;
 
 @SpringBootApplication
 public class Application extends org.springframework.boot.web.support.SpringBootServletInitializer {
+    @Autowired
+    private Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -20,9 +24,9 @@ public class Application extends org.springframework.boot.web.support.SpringBoot
         HikariDataSource ds = new HikariDataSource();
         ds.setMaximumPoolSize(100);
         ds.setDataSourceClassName("com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
-        ds.addDataSourceProperty("url", "jdbc:mysql://localhost:3306/test");
-        ds.addDataSourceProperty("user", "root");
-        ds.addDataSourceProperty("password", "password");
+        ds.addDataSourceProperty("url", env.getProperty("spring.datasource.url"));
+        ds.addDataSourceProperty("user", env.getProperty("spring.datasource.username"));
+        ds.addDataSourceProperty("password", env.getProperty("spring.datasource.password"));
         ds.addDataSourceProperty("cachePrepStmts", true);
         ds.addDataSourceProperty("prepStmtCacheSize", 250);
         ds.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
